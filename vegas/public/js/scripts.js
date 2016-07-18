a712c0c8804129a9757c4ff4435824d2e8c0dd1e
Pauline Hilborn <philbo130@gmail.com>
2016-07-18 13:41:00 -0400

    adding google map and map markers

--- vegas/public/js/scripts.js
+++ vegas/public/js/scripts.js
@@ -1,6 +1,8 @@
 console.log("pearson goes vegas");
 $(document).ready(function() {
 
+//////////////Countdown Timer///////////////
+
 /* jQuery grabber variables */
 var $days = $('#days');
 var $hours = $('#hours');
@@ -38,3 +40,48 @@ function initializeClock(){
 initializeClock(deadline);
 
 }); //<--closes out the document ready jQuery
+
+////////////Map and Map Markers///////////////
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 36.1212, lng: -115.1697}
+  });
+
+  setMarkers(map);
+}
+
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
+var locations = [
+  ['Bellagio', 36.114, -115.172751, 4],
+  ['e by Jose Andreas', 36.1097544, -115.17387259999998, 3],
+  ['XS', 36.1265953, -115.1653652, 2],
+  ['Venetian Sports Book', 36.124302, -115.168613, 1],
+];
+
+function setMarkers(map) {
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+
+  for (var i = 0; i < locations.length; i++) {
+    var location = locations[i];
+    var marker = new google.maps.Marker({
+      position: {lat: location[1], lng: location[2]},
+      map: map,
+      shape: shape,
+      icon: '/img/hopmarker.png',
+      title: location[0],
+      zIndex: location[3],
+    });
+  }
+}
+
+
+
+
+
+
