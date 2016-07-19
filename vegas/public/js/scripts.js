0c1b97b60b5b41cf7b34fddacf27e5998c8582ec
Pauline Hilborn <philbo130@gmail.com>
2016-07-19 14:44:46 -0400

    creating quiz

--- vegas/public/js/scripts.js
+++ vegas/public/js/scripts.js
@@ -39,6 +39,180 @@ function initializeClock(){
 
 initializeClock(deadline);
 
+////////////////Trivia Quiz/////////////////
+
+(function() {
+  var questions = [{
+    question: "What were the names of the two cats who lived at 290 Pearson Street?",
+    choices: ["Maru and Grumpy Cat", "Baby Joey and Zoe", "Milo and Otis", "Cole and Marmalade"],
+    correctAnswer: 1
+  }, {
+    question: "Who was the only rent-paying, female resident?",
+    choices: ["Holly Golightly", "Holly Madison", "Holly Avurnin", "Holly Wood"],
+    correctAnswer: 2
+  }, {
+    question: "Who relieved himself on the entertainment center?",
+    choices: ["Marc Vanderkeere", "Brad Pitser", "Alex Hawkins","Dave Posch"],
+    correctAnswer: 2
+  }, {
+    question: "Whose girlfriend exclaimed, <q>Oh! I get it! You make words!</q> while observing a game of Scrabble?",
+    choices: ["Brad Pitser", "Dave Posch","Alex Hawkins", "Brad Hilborn"],
+    correctAnswer: 1
+  }, {
+    question: "Bonus question - What was her occupation?",
+    choices: ["Etymologist", "Waitress", "Hair Stylist", "Stripper"],
+    correctAnswer: 3
+  }, {
+    question: "What is 1 + 1?",
+    choices: [1, 2, 3, 4],
+    correctAnswer: 1
+  }];
+  
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#quiz'); //Quiz div object
+  
+  // Display initial question
+  displayNext();
+  
+  // Click handler for the 'next' button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+    
+    // Suspend click listener during fade animation
+    if(quiz.is(':animated')) {        
+      return false;
+    }
+    choose();
+    
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+  
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+  
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+  
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+  
+  // Creates and returns the div that contains the questions and 
+  // the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li class="answers">');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        }
+        
+        // Controls display of 'prev' button
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+          
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+  
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] == questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+    
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right.');
+    return score;
+  }
+})();
+
 }); //<--closes out the document ready jQuery
 
 ////////////Map and Map Markers///////////////
@@ -48,7 +222,6 @@ function initMap() {
     zoom: 13,
     center: {lat: 36.1212, lng: -115.1697}
   });
-
   setMarkers(map);
 }
 
@@ -79,9 +252,6 @@ function setMarkers(map) {
     });
   }
 }
-
-
-
 
 
 
