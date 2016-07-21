3abc54f134837473245a9c0c118e93a12fa6e6ba
Pauline Hilborn <philbo130@gmail.com>
2016-07-21 14:54:46 -0400

    adding procfile and updating for heroku

--- vegas/server.js
+++ vegas/server.js
@@ -1,19 +1,24 @@
-var express        = require('express'),
-    bodyParser     = require('body-parser'),
-    mongoose       = require('mongoose'),
-    port           = 3000 || process.env.PORT,
-    app            = express();
 
-mongoose.connect('mongodb://localhost:27017/pearson_vegas');
+//REQUIREMENTS
+var mongoUri 		= process.env.MONGOLAB_URI || 'mongodb://localhost/pearson_vegas',
+	express      	= require('express'),
+    bodyParser     	= require('body-parser'),
+    mongoose       	= require('mongoose'),
+    port           	= process.env.PORT || 3000,
+    app            	= express();
 
+// MIDDLEWARE
+// mongoose.connect('mongodb://localhost:27017/pearson_vegas');
+mongoose.connect(mongoUri);
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
 
+// CONTROLLERS
 var attendeesController = require('./controllers/attendeesController.js');
 app.use('/attendees', attendeesController);
 
+// LISTEN
 mongoose.connection.once('open', function(){
 app.listen(port, function() {
     console.log('Running on port ' + port);
